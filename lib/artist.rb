cb4207d355d723bdb57e68b29d2349042316669a
teasacura <teasacuraster@gmail.com>
2018-04-25 22:37:52 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+require "pry"
+
+class Artist
+  attr_accessor :name
+
+  def initialize
+    @name = name
+  end
+  
+end
+
+# beyonce = Artist.new
+# beyonce.name = "Beyonce"
+#
+# binding.pry
+# true
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+  attr_accessor :title, :artist
+
+  def initialize
+    @title = title
+    @artist = Artist
+  end
+
+end
